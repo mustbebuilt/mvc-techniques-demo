83be638df4c74e3f3e1eeee0d1bb59e47f927f4d
martin@mustbebuilt.co.uk <martinmustbebuilt@gmail.com>
2021-01-21 10:01:00 +0000

    New form model

---
+++ Models/FeedbackForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace justAPlayMVC.Models
+{
+    public class FeedbackForm
+    {
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string Telephone { get; set; }
+
+        public string Message { get; set; }
+
+        public string RedirectMe { get; set; }
+    }
+}
